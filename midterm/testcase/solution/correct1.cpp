81436fca40b2b25afa5e1e9f6dad6e1926f1d823
mjlee <b10902113@csie.ntu.edu.tw>
2024-04-23 12:37:20 +0800

    testcases

--- midterm/testcase/solution/correct1.cpp
+++ midterm/testcase/solution/correct1.cpp
@@ -1,9 +1,29 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
+int checkNum(char *str){
+	int len = strlen(str);
+	int value = 0;
+	for(int i = len - 1;i >= 0;i--){
+		value *= 10;
+		value += str[i] - '0';
+		if(str[i] < '0' || str[i] > '9')
+			return -1;
+	}
+	return value;
+}
+
 int main() {
-	int n;
-	cin >> n;
-	cout << 0 << endl;
-	return 0;
+	char temp[64];
+	bool hasBookNum = false, hasUserNum = false;
+	int bookNum = -1, userNum = -1;
+	while(scanf("%s", temp) != EOF){
+		char none;
+		while (scanf("%c", &none) != EOF && none != '\n'){
+			scanf("%s", temp);
+			if(!hasBookNum){
+				bookNum = checkNum(temp);
+			}
+		}
+	}
 }
