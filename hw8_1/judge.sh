2b1985af247271f4450c0f7987bddbae69ce82eb
mjlee <b10902113@csie.ntu.edu.tw>
2024-04-23 08:11:15 +0800

    midterm

--- hw8_1/judge.sh
+++ hw8_1/judge.sh
@@ -6,13 +6,19 @@ java Main "hw8_1_testcase/tests/0-01.in" > temp.out
 diff temp.out "hw8_1_testcase/tests/0-01.out"
 
 for((i=1;i<10;i++)); do
+    echo "${i}"
     java Main "hw8_1_testcase/tests/1-0${i}.in" > temp.out
     diff temp.out "hw8_1_testcase/tests/1-0${i}.out"
+    echo ""
+    echo ""
 done
 
 for((i=10;i<=25;i++)); do
+    echo "${i}"
     java Main "hw8_1_testcase/tests/1-${i}.in" > temp.out
-    diff temp.out "hw8_1_testcase/tests/1-0${i}.out"
+    diff temp.out "hw8_1_testcase/tests/1-${i}.out"
+    echo ""
+    echo ""
 done
 
 
