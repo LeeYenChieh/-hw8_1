28b65b691df7b67bce5fcc441576fbf574d379d6
mjlee <b10902113@csie.ntu.edu.tw>
2024-04-30 22:07:42 +0800

    main

--- hw9_3/Main.java
+++ hw9_3/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) throws java.io.IOException{
+        FillHandler fillHandler = new FillHandler();
+        BoilHandler boilHandler = new BoilHandler();
+        DrainHandler drainHandler = new DrainHandler();
+        fillHandler.setNextHandler(boilHandler);
+        boilHandler.setNextHandler(drainHandler);
+        drainHandler.setNextHandler(fillHandler);
+
+        Boiler boiler = Boiler.getBoilerInstance();
+        boiler.setHandler(fillHandler);
         try{
             java.io.File inputFile = new java.io.File(args[0]);
             java.io.BufferedReader reader = new java.io.BufferedReader(new java.io.FileReader(inputFile));
@@ -7,13 +16,7 @@ public class Main {
 
             while((line = reader.readLine()) != null){
                 String[] buffer = line.split(" ");
-                if(buffer[0].equals("Fill")){
-                    
-                } else if(buffer[0].equals("Boil")){
-                    
-                } else if(buffer[0].equals("Drain")){
-                    
-                }
+                boiler.operation(buffer[0]);
             }
         } catch (Exception ex) {
             // System.out.println(ex.getMessage());
